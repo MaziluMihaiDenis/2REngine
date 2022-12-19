2aed58945bdf4b7b381937b7afb8aacc48f19861
BoxeHub <75496812+BoxeHub@users.noreply.github.com>
2022-12-19 20:40:18 +0200

    mai multa frustrare

--- 2REngine/frustrare.h
+++ 2REngine/frustrare.h
@@ -1,4 +1,12 @@
-/*
-	Aici va fi toata frustrarea din codul acesta minunat
+//	Aici va fi toata frustrarea din codul acesta minunat
 
-*/
+// For the sins I am about to commit, may James Gosling forgive me
+
+// I dedicate all this code, all my work, to my wife, Darlene,
+// who will have to support me and our three children and
+// the dog once it gets released into the public.
+
+// sometimes I believe compiler ignores all my comments
+
+// When I wrote this, only God and I understood what I was doing
+// Now, God only knows
