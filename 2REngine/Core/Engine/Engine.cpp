296b1d8e046a923cde3bf1da9008919fa7e61d07
Mazilu Mihai Denis <mazilu13.mihaidenis@gmail.com>
2023-02-12 12:10:11 +0200

    Initializes the engine and creates config files

--- 2REngine/Core/Engine/Engine.cpp
+++ 2REngine/Core/Engine/Engine.cpp
@@ -1,8 +1,21 @@
 #include "Engine.h"
+#include "WindowManager.h"
+#include "../../2relibrary.h"
+#include "../../Paths.h"
+
+void Engine::FirstStart()
+{
+	sys_mkdir("C:\\Users\\mazil\\Documents\\mylifebelike", "Config");
+	sys_write_file(CONFIG_PATH, "%s%d", "window\\width = ", 800);
+	sys_write_file(CONFIG_PATH, "%s%d", "window\\height = ", 600);
+	sys_write_file(CONFIG_PATH, "%s%s", "window\\title = ", "MyLifeBeLike");
+}
 
 void Engine::Begin()
 {
-
+	if (sys_mkdir("C:\\Users\\mazil\\Documents\\", "mylifebelike"))
+		FirstStart();
+	WindowManager::GetInstance()->InitWindow();
 }
 
 void Engine::Loop()
@@ -11,4 +24,11 @@ void Engine::Loop()
 
 void Engine::End()
 {
+}
+
+Engine* Engine::GetInstance()
+{
+	if (!Instance)
+		Instance = new Engine();
+	return Instance;
 }
--- 2REngine/Core/Engine/Engine.h
+++ 2REngine/Core/Engine/Engine.h
@@ -5,8 +5,14 @@ class Engine
 	friend int main();
 
 private:
+	inline static Engine* Instance;
+
+private:
+	void FirstStart();
 	void Begin();
 	void Loop();
 	void End();
+public:
+	static Engine* GetInstance();
 };
 
