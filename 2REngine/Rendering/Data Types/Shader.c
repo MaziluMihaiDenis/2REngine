d536cec845dca7c9551b3d86f8d2467f52ad87b1
Mazilu Mihai Denis <mazilu13.mihaidenis@gmail.com>
2023-03-16 21:57:02 +0200

    Idk

--- 2REngine/Rendering/Data Types/Shader.c
+++ 2REngine/Rendering/Data Types/Shader.c
@@ -2,36 +2,40 @@
 #include "../../External/Glad/glad.h"
 #include "../../Core/Types/Filesystem.h"
 #include "../../Core/Types/Debug/debug.h"
+#include "../../Core/Types/RECore.h"
 
-ShaderProgram generate_shader(const char* vertexFn, const char* fragmentFn)
+ShaderProgram* _generate_shader(const char* vertexFn, const char* fragmentFn)
 {
-    ShaderProgram shader = {0};
+    ShaderProgram* shader;
     unsigned int vertID, fragID;
     int ok = 0;
 
+    if (!MALLOC(shader, sizeof(ShaderProgram)))
+        return NULL;
+
     vertID = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(shader.ID, 1, get_file_contents(vertexFn), 0);
-    glCompileShader(shader.ID);
+    glShaderSource(shader->ID, 1, get_file_contents(vertexFn), 0);
+    glCompileShader(shader->ID);
     glGetShaderiv(vertID, GL_COMPILE_STATUS, &ok);
 
     if (!ok)
         PRINT(0, "%s", "Vertex Shader Could Not Be Compiled");
 
 
-    glAttachShader(shader.ID, vertID);
+    glAttachShader(shader->ID, vertID);
 
     fragID = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(shader.ID, 1, get_file_contents(fragmentFn), 0);
-    glCompileShader(shader.ID);
+    glShaderSource(shader->ID, 1, get_file_contents(fragmentFn), 0);
+    glCompileShader(shader->ID);
     glGetShaderiv(fragID, GL_COMPILE_STATUS, &ok);
 
     if (!ok)
         PRINT(0, "%s", "Fragment Shader Could Not Be Compiled");
 
-    glAttachShader(shader.ID, fragID);
+    glAttachShader(shader->ID, fragID);
 
-    glLinkProgram(shader.ID);
-    glGetProgramiv(shader.ID, GL_LINK_STATUS, &ok);
+    glLinkProgram(shader->ID);
+    glGetProgramiv(shader->ID, GL_LINK_STATUS, &ok);
 
     if (!ok)
         PRINT(0, "%s", "Program Could Not Link");
